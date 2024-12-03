2184d94b39565b8f7756f663ecb8eb5104b01579
grimflowers <grimflowers7@gmail.com>
2024-12-03 12:37:56 -0800

    add start button and global states

--- cpe_301_final.ino
+++ cpe_301_final.ino
@@ -3,9 +3,11 @@
 #include "DHT.h"
 #include "RTClib.h"
 
+// Water Level Sensor
 int sensorPin = A0;
 int sensorValue = 0;
 
+// Stepper Motor
 int stepPin1 = 10;
 int stepPin2 = 11;
 int stepPin3 = 12;
@@ -13,19 +15,32 @@ int stepPin4 = 13;
 const int stepsPerRevolution = 2038;
 Stepper myStepper = Stepper(stepsPerRevolution, stepPin1, stepPin3, stepPin2, stepPin4);
 
+// LCD
 const int RS = 8, EN = 9, D4 = 4, D5 = 5, D6 = 6, D7 = 7;
 LiquidCrystal lcd(RS, EN, D4, D5, D6, D7);
 
+// Humidity and Temp sensor
 #define DHT11_PIN 24
 #define DHTTYPE DHT11
 DHT dht(DHT11_PIN, DHTTYPE);
 
+// Clock Cicuit
 RTC_DS1307 rtc;
 char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday",
 "Friday", "Saturday"};
 
+// Global States
+enum State {
+  DISABLED,
+  IDLE,
+  RUNNING,
+  ERROR
+};
+volatile enum State currentState = DISABLED;
+
+const byte startButtonPin = 3; 
+
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   lcd.begin(16, 2);
 
@@ -37,6 +52,9 @@ void setup() {
   dht.begin();
 
   rtc.begin();
+
+  pinMode(startButtonPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(startButtonPin), toggleDisable, FALLING);
 }
 
 void loop() {
@@ -63,4 +81,10 @@ void loop() {
   // Serial.println(now.second(), DEC);
   // Serial.println();
   // delay(3000);
+  Serial.println(currentState);
+  delay(100);
+}
+
+void toggleDisable() {
+  currentState = (currentState == DISABLED) ? IDLE : DISABLED;
 }
